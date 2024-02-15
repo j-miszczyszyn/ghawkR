e7edefdb021043c7ed3d13b11b89f24140f5987f
j-miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-15 17:37:03 +0100

    fix error

--- R/thin_records.R
+++ R/thin_records.R
@@ -7,6 +7,7 @@
 #' @export
 #'
 #' @examples
-thin_records=function(x,y){
-  x=seq(1, nrow(x), y)
-  }
+thin_records <- function(x, y) {
+  selected_rows = seq(1, nrow(x), y)
+  return(x[selected_rows, ])
+}
