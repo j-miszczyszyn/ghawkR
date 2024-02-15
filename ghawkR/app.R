ec5ddc81a299559e2a84d23fc0fc51026f8456de
j-miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-16 00:54:16 +0100

    Update in App

--- ghawkR/app.R
+++ ghawkR/app.R
@@ -1,20 +1,19 @@
 library(shiny)
 library(ghawkR)
-# Załaduj potrzebne pakiety, jeśli korzystasz z dodatkowych funkcji
+library(sf)
 
-options(shiny.maxRequestSize=100*1024^2) # Ustaw limit rozmiaru pliku na 100 MB
+options(shiny.maxRequestSize=100*1024^2)
 
 ui <- fluidPage(
-  titlePanel("Prosta aplikacja Shiny z przyciskiem akcji"),
+  titlePanel("App for plotting flight data"),
   sidebarLayout(
     sidebarPanel(
       fileInput('file1', 'Wybierz plik TXT'),
-      sliderInput('value',
-                  'Wybierz wartość:',
-                  min = 1,
-                  max = 5000,
-                  value = 10),
-      actionButton("goButton", "Przetwarzaj")
+      sliderInput('value', 'Wybierz wartość odchudzania n-th:', min = 1, max = 1000, value = 50),
+      actionButton("goButton", "Process"),
+      textInput("folderPath", "Ścieżka do folderu zapisu:", value = "wpisz ścieżkę folderu"),
+      textInput("fileName", "Nazwa pliku wynikowego:", value = "wynik"),
+      actionButton("exportButton", "Save SHP")
     ),
     mainPanel(
       plotOutput('plot')
@@ -23,22 +22,43 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  observeEvent(input$goButton, {
-    req(input$file1) # Zapewnia, że przetwarzanie rozpocznie się tylko po wczytaniu pliku
-    df <- read.table(input$file1$datapath) # Dostosuj do formatu pliku
+  values <- reactiveValues(df = NULL)
 
-    # Tutaj umieść logikę przetwarzania danych, np. filtrowanie, obliczenia statystyczne itp.
-    df=ghawkR::colnames_Trajectory(df)
-    df=ghawkR::table_to_spatial(df, coords = c(2,3), remove=FALSE)
-    df=ghawkR::thin_records(df, input$value)
-    # Możesz również dodać logikę generowania wykresu, jeśli dane są odpowiednie
+  observeEvent(input$goButton, {
+    req(input$file1)
+    df <- read.table(input$file1$datapath)
+    df <- ghawkR::colnames_Trajectory(df)
+    df <- ghawkR::table_to_spatial(df, coords = c(2,3), remove=FALSE)
+    df <- ghawkR::thin_records(df, input$value)
+    values$df <- df
+
     output$plot <- renderPlot({
-      # Zakładając, że df to Twój przetworzony dataframe
-      if (nrow(df) > 0) {
-        ghawkR::plot_points_coord(df) # Przykładowe wykorzystanie wartości suwaka
+      if (nrow(values$df) > 0) {
+        ghawkR::plot_points_coord(values$df)
       }
     })
+  })
+
+  observeEvent(input$exportButton, {
+    req(values$df, input$folderPath, input$fileName)
+    filePath <- paste0(input$folderPath, "/", input$fileName, ".shp")
+    tryCatch({
+      st_write(values$df, filePath)
+      showModal(modalDialog(
+        title = "Sukces",
+        "Plik został zapisany.",
+        easyClose = TRUE,
+        footer = NULL
+      ))
+    }, error = function(e) {
+      showModal(modalDialog(
+        title = "Błąd",
+        paste("Nie udało się zapisać pliku:", e$message),
+        easyClose = TRUE,
+        footer = NULL
+      ))
     })
+  })
 }
 
 shinyApp(ui = ui, server = server)
