844c11a121e36e6a338efe67dd14690e6d87cb93
j-miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-15 17:03:57 +0100

    function for sample data

---
+++ R/sample_data.R
@@ -0,0 +1,12 @@
+#' Example data
+#'
+#' Example data created manually for the purpose of this package.
+#'
+#' @format ## `sample_data`
+#' A data frame with 10 observations on 2 variables.
+#' \describe{
+#' \item{x}{numeric vector}
+#' \item{y}{numeric vector}
+#' }
+#' @source Manually created.
+"sample_data"
