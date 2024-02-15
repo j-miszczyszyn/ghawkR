5fa1e8fcc3af59b1fd09b88da4ca46c8a9e20f9b
j-miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-15 18:53:30 +0100

    PLoting chart of merged data with color depended form flight number

---
+++ R/plot_points_coord_merged.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_points_coord_merged=function(x){
+  plot(x=x$`East Coordinates (m)`, y=x$`North Coordinates (m)`, col=test$FLIGHT)
+}
